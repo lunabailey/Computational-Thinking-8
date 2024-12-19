651cec824d9bf234971766b9eec224d99e71506a
lunabailey <lunabailey@seattleacademy.org>
2024-12-19 22:10:28 +0000

    turning in project 2

---
+++ Projects/grandma.py
@@ -0,0 +1,9 @@
+while True:
+    word = input("what do you think grandma likes?")
+    
+    if "t" in word 
+    print(f"Grandma doesn't like tea")
+else:
+    print(f"Grandma likes like coffee")
+
+    print("")
